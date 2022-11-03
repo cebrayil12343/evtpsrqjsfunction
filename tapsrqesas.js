0ca4ca216492e08b73ba0cb06c06b2f890474295
cebrayil12343 <tu2azbdnd@code.edu.az>
2022-11-03 06:04:20 +0400

    update commit

---
+++ tapsrqesas.js
@@ -0,0 +1,24 @@
+function sum(a, b){
+    return a + b
+}
+let sedr = sum(7, 5)
+console.log(sedr)
+
+function multiply(a, b){
+    return a * b
+}
+let net = multiply(7, 5)
+console.log(net)
+
+
+function substract(a, b){
+    return a - b
+}
+let retrn = substract(7, 5)
+console.log(retrn)
+
+function divide(a, b){
+    return a / b
+}
+let rotrn = divide(7, 5)
+console.log(rotrn)
